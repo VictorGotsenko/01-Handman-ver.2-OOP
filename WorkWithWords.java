777d5fe94f3727cfcf9c15e5692e18bc97be95cd
Victor <victor.gotsenko@gmail.com>
2024-03-12 20:37:35 +0500

    write and test metods: readWordsFromFile, wordsFiltering

--- WorkWithWords.java
+++ WorkWithWords.java
@@ -1,24 +1,30 @@
+/**
+ * Work With Words
+ */
+
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-/**
- * Work With Words
- */
 public class WorkWithWords {
-    // read words from file
-    ArrayList<String> readWordsFromFile() {
-        String sWorkDir = "src";
-        String sWordsDictionary = "WordsDictionary.txt";
-        String sTmpString = "а";
-        ArrayList<String> wordsRawDictionary = new ArrayList<>();
+    ArrayList<String> wordsDictionary = new ArrayList<>();
+    int maxWordLenght = 10;
+    int minWordLenght = 5;
+    //    read words from file
+    void readWordsFromFile() {
+        String workDir = "src";
+        String fileNameDictionaryWords = "WordsDictionary.txt";
+        String readedString = "а";
         try {
-            File file = new File(String.valueOf(Paths.get(sWorkDir, sWordsDictionary).toFile()));
+            File file = new File(String.valueOf(Paths.get(workDir, fileNameDictionaryWords).toFile()));
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
-            while ((sTmpString = reader.readLine()) != null) {                                                          // считываем строки в цикле
-                sTmpString = sTmpString.toLowerCase();
-                wordsRawDictionary.add(sTmpString);
+            while ((readedString = reader.readLine()) != null) { // читаю строки в цикле
+                readedString = readedString.toLowerCase();
+                readedString = wordsFiltering(readedString);
+                if (!(readedString == null)) {
+                    wordsDictionary.add(readedString);
+                }
             }
             reader.close();
             fr.close();
@@ -27,7 +33,38 @@ public class WorkWithWords {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return wordsRawDictionary;
     }
-
-}
+    String wordsFiltering(String wordsRawDictionary) {
+    //   сколько слов в строке?
+        String[] words = wordsRawDictionary.split(" ");
+        if (words.length > 1) {
+            System.out.println("В строке словаря обнаружено более одного слова. Обработаю только первое слово: " + words[0]
+                    + ". Остальные слова в строке будут отброшены.");
+        }
+        String firstWord = words[0];
+    //    обработка слова (менее 5 или более 10  букв отбросить)
+        if ((firstWord.length() < minWordLenght) || (firstWord.length() > maxWordLenght)) {
+            System.out.println("Слово: " + firstWord
+                    + " - не подходит под диапазон от 5 до 10 букв. И было отброшено.");
+            return firstWord = null;
+        }
+    //    проверка - слово содержит только русские буквы ?
+        char tempChap;
+        for (int i = 0; i < firstWord.length(); i++) {
+            tempChap = firstWord.charAt(i);
+            if (!(tempChap >= 'а' && tempChap <= 'я')) {
+                if (tempChap == 'ё') {
+                    continue;
+                } else {
+                    System.out.println("Слово: " + firstWord + " содержит недопустимые символы и было отброшено ");
+                    return firstWord = null;
+                  }
+            }
+        }
+        return firstWord;
+    }
+public static void main(String[] args) {
+    WorkWithWords wordwork = new WorkWithWords();
+    wordwork.readWordsFromFile();
+ }
+} //EoClass
